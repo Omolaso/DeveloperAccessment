db03a553204e8be74d1330432946a71c2a882360
omolaso <adextee001@gmail.com>
2022-11-11 05:04:38 +0100

    updates

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const firstName = document.getElementById('first-name');
+const firstNameValue = firstName.value.trim();
+
 const lastName = document.getElementById('last-name');
+const lastNameValue = lastName.value.trim();
+
 const email = document.getElementById('email');
+const emailValue = email.value.trim();
+
 const number = document.getElementById('number');
+const numberValue = number.value.trim();
+
 const product = document.getElementById('product');
 const business = document.getElementById('business');
 const monthlySales = document.getElementById('monthly-sales');
-const radioYes = document.getElementById('yes');
-const radioNo = document.getElementById('no');
-const checkbox = document.getElementById('checkbox');
+
+
+const submitBtn = document.getElementById('submit')
+const form = document.getElementById('form');
 
 
 
@@ -16,129 +25,182 @@ const checkbox = document.getElementById('checkbox');
 
 
 
-const submitBtn = document.getElementById('form');
-submitBtn.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(setSuccessFor === true){
-        console.log('hi');
-     }else{
-        // localStorage.setItem('First Name', `${firstNameValue}`);
-        // localStorage.setItem('Last Name', `${lastNameValue}`)
-        console.log('hey');
-     }
-
-    const firstNameValue = firstName.value.trim();
-    if(firstNameValue === ''){
-        setErrorFor(firstName, 'First name cannot be blank')
-    }else if (firstNameValue.length <= 2) {
-        setErrorFor(firstName, 'Must be above 3 letters')
+    //firstname
+    if(!firstNameValue){
+        setErrorFor(firstName, 'First name cannot be blank');
     }
     else{
-        setSuccessFor(firstName)
+        setErrorFor(firstName, '');
     };
 
-    const lastNameValue = lastName.value.trim();
-    if(lastName.value === ''){
+    //lastname
+    if(!lastNameValue){
         setErrorFor(lastName, 'Last name cannot be blank');
-    }else if (lastNameValue.length <= 2) {
-        setErrorFor(lastName, 'Must be above 3 letters')
     }
     else{
-        setSuccessFor(lastName)
+        setErrorFor(lastName, '');
     };
 
-    const numberValue = number.value.trim();
-    if(number.value === ''){
-        setErrorFor(number, 'Include a phone number');
-    }else if(numberValue.length <= 10 || numberValue.length > 15 ){
-        setErrorFor(number, 'Invalid Phone Number')
+    //phone-number
+    if(!numberValue){
+        setErrorFor(number, 'Include a phone number. Format: xxxx-xxx-xxxx');
+    }else if(!validPhoneNumber(numberValue)){
+        setErrorFor(number, 'Invalid Phone Number. Format: xxxx-xxx-xxxx')
     }
     else{
-        setSuccessFor(number);
-        localStorage.setItem('Phone Number', `${numberValue}`)
+        setErrorFor(number, '');
     }
 
-    const emailValue = email.value.trim();
-    if(email.value === ''){
+    //email
+    if(!emailValue){
         setErrorFor(email, 'Email cannot be blank')
-    }else if(!isEmail(emailValue)){
+    }else if(!validEmail(emailValue)){
         setErrorFor(email, 'Invalid Email')
     }
     else{
-        setSuccessFor(email);
-        localStorage.setItem('Email Address', `${emailValue}`)
+        setErrorFor(email, '')
     }
 
-    const productValue = product.value.trim();
-    if(product.value === ''){
-        setErrorFor(product, 'Please select a location')
-    }else{
-        setSuccessFor(product);
-        localStorage.setItem('Where Do You Sell', `${productValue}`)
-    }
-
-    const monthlySalesValue = monthlySales.value.trim();
-    if(monthlySales.value === ''){
-        setErrorFor(monthlySales, 'Please specify your monthly sales')
-    }else{
-        setSuccessFor(monthlySales)
-        localStorage.setItem('Average Monthly Sales', `${monthlySalesValue}`)
-    }
-
-    const businessValue = business.value.trim();
-    if(business.value === ''){
-        setErrorFor(business, 'Please specify your business type')
-    }else{
-        setSuccessFor(business)
-        localStorage.setItem('Business Type', `${businessValue}`)
-    }
-
-//   console.log(checkbox.checked)
-
-    if(checkbox.checked === true){
-        localStorage.setItem('Agree to our terms?', 'Yes')
-        // setErrorFor(checkbox, '')
-        // console.log('true');
-    }
-    // else{
-    //    setErrorFor(checkbox, 'Please check the box')
+    // const productValue = product.value.trim();
+    // if(product.value === ''){
+    //     setErrorFor(product, 'Please select a location')
+    // }else{
+    //     setSuccessFor(product);
+    //     localStorage.setItem('Where Do You Sell', `${productValue}`)
     // }
 
-    // const radioButtons = document.querySelectorAll('input[name ="interest"]');
-    // for (const radioButton of radioButtons) {
-    //     if(radioButton){
-    //         localStorage.setItem('Interested in business management software?', `${radioButton.value}`)
-    //     }
+    // const monthlySalesValue = monthlySales.value.trim();
+    // if(monthlySales.value === ''){
+    //     setErrorFor(monthlySales, 'Please specify your monthly sales')
+    // }else{
+    //     setSuccessFor(monthlySales)
+    //     localStorage.setItem('Average Monthly Sales', `${monthlySalesValue}`)
     // }
+
+    // const businessValue = business.value.trim();
+    // if(business.value === ''){
+    //     setErrorFor(business, 'Please specify your business type')
+    // }else{
+    //     setSuccessFor(business)
+    //     localStorage.setItem('Business Type', `${businessValue}`)
+    // }
+
+    finalSubmit();
 });
 
 
 
-// FUNCTIONS
-
-function setErrorFor(variable, message){
-    //note: the variable parameter is for EACH inputs that is why it can be changed when called.
-
-    const formInput = variable.parentElement;
+// VALIDATIONS
+function setErrorFor(input, message){
+    const formInput = input.parentElement;
     const small = formInput.querySelector('small');
-    small.innerText = message
-    //error class
-    formInput.classList = 'formInput error';
+    small.innerText = message;
+    small.style.color = '#e74c3c';
+
+    if(!small.innerText){
+        formInput.classList = 'formInput success';
+    }else{
+        formInput.classList = 'formInput error';
+    }
+};
+
+
+// email regex
+function validEmail(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,4}))$/.test(email);
 }
 
-function setSuccessFor(variable){
-    const formInput = variable.parentElement;
-    formInput.classList = 'formInput success';
+//phone number regex
+function validPhoneNumber(number) {
+    // return /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/.test(number);
+    return /[0-9]{4}-[0-9]{3}-[0-9]{4}/.test(number);
 }
-
-// email validation
-function isEmail(email){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
-
-//phone validation
-// function isPhone(number) {
-//     return /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
-// }
     
+
+//ONKEYUP && ONBLUR EVENTS
+// email
+function emailChangedValue(){
+
+    if(!emailValue){
+        setErrorFor(email, 'Email cannot be blank');
+        submitBtn.disabled = true;
+        // console.log('You cant submit');
+    }
+    else if(!validEmail(emailValue)){
+        setErrorFor(email, 'Invalid Email');
+        submitBtn.disabled = true;
+        // console.log('You cant submit');
+    }
+    else{
+        setErrorFor(email, '');
+        submitBtn.disabled = false;
+        console.log("You can submit");
+    }
+};
+
+//phone
+function numberChangedValue(){
+
+    if(!numberValue){
+        setErrorFor(number, 'Include a phone number. Format: xxxx-xxx-xxxx');
+        submitBtn.disabled = true;
+        // console.log('You cant submit');
+    }
+    else if(!validPhoneNumber(numberValue)){
+        setErrorFor(number, 'Invalid Phone Number. Format: xxxx-xxx-xxxx');
+        submitBtn.disabled = true;
+        // console.log('You cant submit');
+    }
+    else{
+        setErrorFor(number, '');
+        submitBtn.disabled = false;
+        // console.log("You can submit");
+    }
+};
+
+//firstname
+function firstNameValueCheck(){
+    if(!firstNameValue){
+        setErrorFor(firstName, 'Name cannot be blank');
+    }
+    else{
+        setErrorFor(firstName, '');
+    }
+};
+
+function lastNameValueCheck(){
+    if(!lastNameValue){
+        setErrorFor(lastName, 'Name cannot be blank');
+    }
+    else{
+        setErrorFor(lastName, '');
+    }
+};
+
+// VALIDATIONS END
+
+
+function clearFormInputs(){
+    form.reset();
+}
+
+function storeInputValues(){
+    alert('Your details are now stored in the local storage');
+    localStorage.setItem('Email Address', `${email.value}`);
+    localStorage.setItem('Phone Number', `${number.value}`)
+
+};
+
+function finalSubmit(){
+    if(!firstName.value || !lastName.value || !email.value || !number.value){
+        return;
+        // console.log('no');
+    }
+    else{
+        storeInputValues();
+        clearFormInputs();
+    }
+};
